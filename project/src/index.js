d800df1c2d4119419ae976a879dd040899dad315
tanjaslo <tanja.slo@ukr.net>
2021-06-01 13:05:57 +0200

    Adds components for the main page and card.

--- project/src/index.js
+++ project/src/index.js
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const Setting = {
+  PLACES_COUNT: 5,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      placesCount={Setting.PLACES_COUNT}
+    />,
   </React.StrictMode>,
-  document.getElementById('root'));
+  document.getElementById('root'),
+);
