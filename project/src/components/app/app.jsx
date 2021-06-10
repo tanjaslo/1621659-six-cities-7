fa9681d768bdf95a70d3e4e0c0b8a8447ac2937b
tanjaslo <tanja.slo@ukr.net>
2021-06-10 20:42:59 +0200

    Adds mocks and props validation.

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -7,22 +7,24 @@ import FavoritesPage from '../pages/favorites-page/favorites-page';
 import LoginPage from '../pages/login-page/login-page';
 import RoomPage from '../pages/room-page/room-page';
 import NotFoundPage from '../pages/not-found-page/not-found-page';
+import offersProp from '../../prop-types/offers.prop';
+import reviewsProp from '../../prop-types/reviews.prop';
 
-function App({placesCount}) {
+function App({placesCount, offers, reviews}) {
   return (
     <BrowserRouter>
       <Switch>
         <Route path={AppRoute.MAIN} exact>
-          <MainPage placesCount={placesCount} />
+          <MainPage placesCount={placesCount} offers={offers} />
         </Route>
         <Route path={AppRoute.FAVORITES} exact>
-          <FavoritesPage />
+          <FavoritesPage offers={offers} />
         </Route>
         <Route path={AppRoute.LOGIN} exact>
           <LoginPage />
         </Route>
         <Route path={AppRoute.ROOM} exact>
-          <RoomPage />
+          <RoomPage offers={offers} reviews={reviews} />
         </Route>
         <Route>
           <NotFoundPage />
@@ -34,6 +36,8 @@ function App({placesCount}) {
 
 App.propTypes = {
   placesCount: PropTypes.number.isRequired,
+  offers: PropTypes.arrayOf(offersProp).isRequired,
+  reviews: PropTypes.arrayOf(reviewsProp).isRequired,
 };
 
 export default App;
---
+++ project/src/mocks/offers.js
@@ -0,0 +1,174 @@
+const AVATAR_URL = 'https://i.pravatar.cc/128';
+
+const offers = [
+  {
+    bedrooms: 2,
+    city: {
+      location: {
+        latitude: 52.370216,
+        longitude: 4.895168,
+        zoom: 10,
+      },
+      name: 'Amsterdam',
+    },
+    description:
+      'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    goods: [
+      'Wi-Fi',
+      'Heating',
+      'Coffee machine',
+      'Kitchen',
+      'Cable TV',
+      'Fridge',
+    ],
+    host: {
+      avatarUrl: `${AVATAR_URL}?rnd=${Math.random()}`,
+      id: 1,
+      isPro: true,
+      name: 'Angelina',
+    },
+    id: 1,
+    images: [
+      'img/apartment-01.jpg',
+      'img/apartment-02.jpg',
+      'img/apartment-03.jpg',
+    ],
+    isFavorite: true,
+    isPremium: true,
+    location: {
+      latitude: 52.35514938496378,
+      longitude: 4.673877537499948,
+      zoom: 8,
+    },
+    maxAdults: 4,
+    previewImage: 'img/apartment-01.jpg',
+    price: 120,
+    rating: 4.8,
+    title: 'Beautiful & luxurious studio at great location',
+    type: 'apartment',
+  },
+  {
+    bedrooms: 5,
+    city: {
+      location: {
+        latitude: 52.370216,
+        longitude: 4.895168,
+        zoom: 10,
+      },
+      name: 'Amsterdam',
+    },
+    description:
+      'An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful.',
+    goods: [
+      'Wi-Fi',
+      'Washing machine',
+      'Towels',
+      'Heating',
+      'Coffee machine',
+      'Baby seat',
+      'Kitchen',
+      'Dishwasher',
+      'Cable TV',
+      'Fridge',
+    ],
+    host: {
+      avatarUrl: `${AVATAR_URL}?rnd=${Math.random()}`,
+      id: 2,
+      isPro: false,
+      name: 'Alex',
+    },
+    id: 2,
+    images: ['img/room.jpg', 'img/studio-01.jpg'],
+    isFavorite: true,
+    isPremium: false,
+    location: {
+      latitude: 52.35514938496378,
+      longitude: 4.673877537499948,
+      zoom: 8,
+    },
+    maxAdults: 6,
+    previewImage: 'img/apartment-02.jpg',
+    price: 140,
+    rating: 4.5,
+    title: 'Large luxury house in a quiet place',
+    type: 'house',
+  },
+  {
+    bedrooms: 1,
+    city: {
+      location: {
+        latitude: 52.370216,
+        longitude: 4.895168,
+        zoom: 10,
+      },
+      name: 'Amsterdam',
+    },
+    description:
+      'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.',
+    goods: ['Wi-Fi', 'Kitchen', 'Dishwasher', 'Cable TV', 'Fridge'],
+    host: {
+      avatarUrl: `${AVATAR_URL}?rnd=${Math.random()}`,
+      id: 3,
+      isPro: true,
+      name: 'Emma',
+    },
+    id: 3,
+    images: ['img/apartment-01.jpg', 'img/room.jpg', 'img/studio-01.jpg'],
+    isFavorite: true,
+    isPremium: false,
+    location: {
+      latitude: 52.35514938496378,
+      longitude: 4.673877537499948,
+      zoom: 8,
+    },
+    maxAdults: 3,
+    previewImage: 'img/apartment-03.jpg',
+    price: 100,
+    rating: 3.3,
+    title: 'Sunny bungalow with a view',
+    type: 'bungalow',
+  },
+  {
+    bedrooms: 1,
+    city: {
+      location: {
+        latitude: 52.370216,
+        longitude: 4.895168,
+        zoom: 10,
+      },
+      name: 'Amsterdam',
+    },
+    description:
+      'The guesthouse is situated in the heart of the city centre within walking distance of museums, the main shopping area and night life. You will find plenty of cafés and restaurants in the area.',
+    goods: [
+      'Wi-Fi',
+      'Towels',
+      'Heating',
+      'Cable TV',
+      'Fridge',
+    ],
+    host: {
+      avatarUrl: `${AVATAR_URL}?rnd=${Math.random()}`,
+      id: 4,
+      isPro: true,
+      name: 'Stanislav',
+    },
+    id: 4,
+    images: ['img/apartment-01.jpg', 'img/room.jpg'],
+    isFavorite: false,
+    isPremium: false,
+    location: {
+      latitude: 52.35514938496378,
+      longitude: 4.673877537499948,
+      zoom: 8,
+    },
+    maxAdults: 2,
+    previewImage: 'img/room.jpg',
+    price: 100,
+    rating: 4.5,
+    title: 'Cozy room at great location',
+    type: 'private room',
+  },
+];
+
+export default offers;
